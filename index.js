2f2063620ebe902c6921643c2ed5f87312b3ba4e
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-05-14 13:14:34 +0300

    Completed Assigntment

---
+++ index.js
@@ -0,0 +1,31 @@
+let number = 5
+function makeInt(x){
+    return parseInt(x,10)
+}
+
+function preserveDecimal(x){
+    return parseFloat(x)
+}
+
+function add(a, b){
+    return (a+b)
+}
+
+function subtract(a, b){
+    return (a-b)
+}
+
+function multiply(a,b){
+    return (a*b)
+}
+
+function divide(a,b){
+    return (a/b)
+}
+function increment(n){
+    return (++n)
+}
+
+function decrement(n){
+    return (--n)
+}
